3d1d8a228b5827d05df10ecc43d590b1e5e10d67
pr-olga <frkhorkovaolga@gmail.com>
2019-09-21 10:54:10 +0200

    feat: add axios, basic api request to client, and basic post to the server
    change port to 8082 because 8081 was in use

--- server/src/app.js
+++ server/src/app.js
@@ -8,10 +8,10 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/status', (req,res) => {
+app.post('/register', (req,res) => {
     res.send({
-        message: "hello pr-olga"
+        message: `Hello ${req.body.email}, your user is registered!`
     })
 });
 
-app.listen(process.env.PORT || 8081)
+app.listen(process.env.PORT || 8082)
