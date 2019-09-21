4893fdb7e97288cb9498e0510cca0a45b0d01da6
pr-olga <frkhorkovaolga@gmail.com>
2019-09-21 11:59:42 +0200

    feat: add async method to client to post data from UI

---
+++ client/src/components/Register.vue
@@ -0,0 +1,49 @@
+<template>
+    <section class="register">
+        <h1>Register</h1>
+
+        <input type="text" name="email" id="" placeholder="email"
+        v-model="email"
+        ><br>
+        <input type="password" name="password" id="" placeholder="password"
+        v-model="password"
+        ><br>
+        <button @click="register">Register</button>
+    </section>
+</template>
+
+<script>
+import AuthentificationService from '@/services/AuthentificationService'
+export default {
+    name: 'Register',
+    data () {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        async register () {
+            const response = await AuthentificationService.register({
+                email: this.email,
+                password: this.password
+            })
+            console.log (response.data)
+        }
+    }
+/*     watch: {
+        email (value) {
+            console.log('email has changed', value)
+        }
+    },
+    mounted () {
+        setTimeout(() => {
+            this.email = 'hello wolrd'
+        }, 2000)
+    } */
+}
+</script>
+
+<style scoped>
+
+</style>
