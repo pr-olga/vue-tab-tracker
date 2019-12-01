5420d4b88488df2f4943111f381a3ea46781c2de
pr-olga <frkhorkovaolga@gmail.com>
2019-12-01 22:22:27 +0100

    feat: part 4: add browse songs component

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -11,8 +11,11 @@
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        <b-nav-item>
+          <router-link to="songs">
+               Browser
+           </router-link>
+        </b-nav-item>
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
---
+++ client/src/components/Songs.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="songs">
+        Olga
+    </section>
+</template>
+
+<script>
+
+export default {
+  name: 'Songs'
+}
+</script>
+
+<style scoped>
+</style>
