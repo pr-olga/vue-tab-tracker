6608b3970cb4e9806652a81085a0ebea492d6fae
pr-olga <frkhorkovaolga@gmail.com>
2019-12-03 19:33:03 +0100

    feat: part 4: add  congs methods to routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,10 +1,15 @@
 const Authenticationcontroller = require('./controllers/AuthenticationController')
 const AuthenticationcontrollerPolicy = require('./policies/AuthenticationControllerPolicy')
+const SongsController = require('./controllers/SongsController')
 
 module.exports = (app) => {
     app.post('/register',
         AuthenticationcontrollerPolicy.register,
         Authenticationcontroller.register),
     app.post('/login',
-        Authenticationcontroller.login)
+        Authenticationcontroller.login),
+    app.get('/songs',
+        SongsController.index)
+    app.post('/songs',
+    SongsController.post)
 }
