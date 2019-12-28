6c975ea6737470ba326b1b45654180e23820959c
pr-olga <frkhorkovaolga@gmail.com>
2019-12-28 20:31:04 +0100

    feat: part 6: add npm command for seed
    fix the bug on index seed

--- server/src/seed/index.js
+++ server/src/seed/index.js
@@ -9,16 +9,16 @@ const songs = require('./songs.json')
 const users = require('./users.json')
 
 sequelize.sync({ force: true })
-  .then(async function {
-     await Promise.all(
-       users.map(user =>{
-         User.create(user)
-       })
-     )
+  .then(async function () {
+    await Promise.all(
+      users.map(user => {
+        User.create(user)
+      })
+    )
 
-     await Promise.all(
-        songs.map(song =>{
-          Song.create(song)
-        })
-      )
+    await Promise.all(
+      songs.map(song => {
+        Song.create(song)
+      })
+    )
   })
